52b93f1ff0998464928671e3e81c53e54b8f1d7c
FanisNgv <fanisnigamad@yandex.ru>
2024-05-15 22:10:51 +0300

    user router repaired

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,7 +4,6 @@ const controller = require('../controllers/userController');
 const {check} = require("express-validator");
 const authMiddleware = require("../middleware/authMiddleware")
 
-<<<<<<< HEAD
 
 router.post('/createTransaction', authMiddleware, controller.createTransaction); // это подроут для /user. Т.е. будем иметь .../user/createTransaction
 router.post('/getTransactions', authMiddleware, controller.getTransactions);
